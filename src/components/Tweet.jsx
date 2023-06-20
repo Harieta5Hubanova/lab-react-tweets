ed1d840c43268ec8daafd5b217f113292eb2d295
Harieta Hubanova <harieta5hubanova@gmail.com>
2023-06-20 22:21:39 +0100

    Almost Done

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,32 @@
+import Message from './Message';
+import ProfileImage from './ProfileImage';
+import Timestamp from './Timestamp';
+import User from './User';
+
+function Tweet(props) {
+  return (
+    <div className="tweet">
+      <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User userData={props.tweet.user} />
+
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
+        </div>
+      </div>
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+export default Tweet;
